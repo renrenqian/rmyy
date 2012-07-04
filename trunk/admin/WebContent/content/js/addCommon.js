08dbd4d7f9577fdae78e91e4d8d3780da77ec523
renrenqian <420264018@qq.com>
2012-07-04 13:29:36 +0800

    add common page init

---
+++ trunk/admin/WebContent/content/js/addCommon.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+     $('#contentForm').sdValidate();//添加验证规则        
+    /* 编辑、新增 确认按钮 */
+    $('#J_ContentOk').die().live("click", function() {
+        if ($(this).sdSubmitValidate("#contentForm")) {
+            var contInfoId = $("#continfo.contId").val();
+            var url,actionName;
+            if (!contInfoId || contInfoId == "") {
+            	actionName = "新增";
+                url = "../group/addContent.action"; 
+            } else {
+            	actionName = "编辑";
+                url = "../group/updateContent.action";
+            }
+        $('#contentForm').form('submit', {
+          url:url,
+          dataType : 'json',
+          onSubmit: function(){            
+         },
+         error:function (json) {
+             alert("内容异常:" + json.message) ; 
+         }, 
+         success:function(json){
+             json = eval('(' + json + ')');
+             if (json.resultCode > 0 ) {               
+                 $(window.parent.document).find("#centerIFrame").attr("src", "content/contentMan.html");
+                } else  {
+                    $.fn.sdInfo({
+                        type : "fail",
+                        content : json.message ? actionName+"内容错误:"+json.message : actionName+"内容错误:"
+                    });
+                }
+         }
+     });  
+        }       
+    });
+
+    /* 编辑、新增 取消按钮 */
+    $('#J_ContentClose').die().live("click", function() {
+        $(window.parent.document).find("#centerIFrame").attr("src", "content/contentMan.html");
+    });
+});
+
+
+//日期控件
+(function(S) {
+    KISSY.use('calendar', function(S) {
+        //创建日期
+        var cPubTime = new S.Calendar('#contPubTime', {
+            popup:true,
+            showTime:true
+        }).on('timeSelect', function(e) {
+            var dateFormat = new DateFormat();
+            $('#contPubTime').val(dateFormat.isoDateTime(e.date));
+            dateFormat = null;
+            cPubTime.hide();
+        });
+        cPubTime.render({minDate:new Date()});
+    });
+})(KISSY);
