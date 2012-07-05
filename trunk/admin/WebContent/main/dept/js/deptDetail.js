90a1577cb6505badda8c685c13d78a3954dfbb2f
zhoumf <zhou_mingfang@stardream.tv>
2012-07-05 11:37:58 +0800

    Add main page.

---
+++ trunk/admin/WebContent/main/dept/js/deptDetail.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	 $.getJSON('http://124.160.67.194:8520/admin/group/searchDept.action?dept.dpId=1031', function(json) {		 		 	
+         if (json.resultCode > 0) {            
+             var deptDetail = json.dept;
+             $(deptDetail).each(function(i, item) {                  
+             	$('.J_DpName').html(item.dpName);   
+             	$('#J_DpIn_charge').html(item.dpIn_charge); 
+             	$('#J_DpBed_counter').html(item.dpBed_counter); 
+             	$('#J_DpLocation').html(item.dpLocation); 
+             	$('#J_DpOd_telephone').html(item.dpOd_telephone); 
+             	$('#J_DpEmail').html(item.dpEmail); 
+             	               
+             	$('#J_DpDesc').html("<p>"+fixTAFormat(reP(item.dpDesc))+"</p>");  
+             	$('#J_DpAcademic_position').html("<p>"+fixTAFormat(reP(item.dpAcademic_position))+"</p>");   
+             	$('#J_DpTech_adv').html("<p>"+fixTAFormat(reP(item.dpTech_adv))+"</p>");   
+             	$('#J_DpResearch_direction').html("<p>"+fixTAFormat(reP(item.dpResearch_direction))+"</p>");                	           	         
+             });     
+         }
+	 });
+});
