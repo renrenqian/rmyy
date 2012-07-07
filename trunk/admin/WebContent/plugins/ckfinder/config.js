465674cc6f0da55020eb3c29d838a112d9efc8be
zhoumf <zhou_mingfang@stardream.tv>
2012-07-08 01:46:31 +0800

    Upload and show the image at the content while add or edit the content, should put the ckeditor and the ckfinder at the root path, same with the index.html path, so the jsp file can upload and preview the image at ckeditor, dear , so sleepy.

--- trunk/admin/WebContent/plugins/ckfinder/config.js
+++ trunk/admin/WebContent/plugins/ckfinder/config.js
@@ -5,42 +5,15 @@ For licensing, see LICENSE.html or http://ckfinder.com/license
 
 CKFinder.customConfig = function( config )
 {
-	// Define changes to default configuration here.
-	// For the list of available options, check:
-	// http://docs.cksource.com/ckfinder_2.x_api/symbols/CKFinder.config.html
+    // Define changes to default configuration here.
+    // For the list of available options, check:
+    // http://docs.cksource.com/ckfinder_2.x_api/symbols/CKFinder.config.html
 
-	// Sample configuration options:
-	// config.uiColor = '#BDE31E';
-	// config.language = 'fr';
-	// config.removePlugins = 'basket';
-	// Define changes to default configuration here. For example:
+    // Sample configuration options:
+    // config.uiColor = '#BDE31E';
+    // config.language = 'fr';
+    // config.removePlugins = 'basket';
+    // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
-    config.language = 'zh-cn'; // 配置语言
-
-    config.uiColor = '#fff'; // 背景颜色
-
-    config.width = '600px'; // 宽度
-
-    config.height = '400px'; // 高度
-
-    // lead the issue
-    //config.skin = 'office2003';// 界面v2,kama,office2003 
-
-    config.toolbar = 'Full';// 工具栏风格Full,Basic
-    
-    config.font_names='宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;' +
-    '隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;'+ config.font_names;
-    
-    config.filebrowserBrowseUrl ='ckfinder.html';
-    
-    config.filebrowserImageBrowseUrl ='ckfinder.html?Type=Images';
-    
-    config.filebrowserFlashBrowseUrl = 'ckfinder.html?Type=Flash';
-    
-    config.filebrowserUploadUrl = 'core/connector/java/connector.java?command=QuickUpload&type=Files';
-    
-    config.filebrowserImageUploadUrl = 'core/connector/java/connector.java?command=QuickUpload&type=Images';
-    
-    config.filebrowserFlashUploadUrl = 'core/connector/java/connector.java?command=QuickUpload&type=Flash';
-};
+ };
